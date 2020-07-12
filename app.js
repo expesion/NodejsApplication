4d0c7c2caf5e704242f1260700777cff97a587fa
Hussine <spcocmain@gmail.com>
2020-07-12 15:06:59 +0530

    Changes made to run heroku

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
 const request = require('request');
-const yargs= require("yargs");
+const express =require('express');
+const path =require('path');
 
-yargs.version('1.1.0');
-
-/* yargs.command({
-    command:'city',
-    describe:'Name of the city',
-    builder:{
-        name1:{
-            describe:'Name of the city'
-        }
-    },
-    handler: function(argv){
-        console.log(argv);
-    }
-}) */
-
- const url =`http://api.weatherstack.com/current?access_key=0274be5cb2f9dae9e7d1560363a62b4d&query=Bangalore&units=m`;
+const path1=path.join(__dirname+'/public')
+const port = process.env.PORT
+console.log(path1);
+const app = express();
+app.use(express.static(path1));
+app.get('',(res,req)=>{
+    req.send('index')
+})
+app.listen(port);
+ /* const url =`http://api.weatherstack.com/current?access_key=0274be5cb2f9dae9e7d1560363a62b4d&query=Bangalore&units=m`;
 request({url:url,json:true},(error,response)=>{
     console.log(`It's currently ${response.body.current.temperature} celcius and has a ${response.body.current.precip} of rain`);
-}); 
+}); */ 
