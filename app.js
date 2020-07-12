2dd4ddf5320921061b97f249779bc0e3cf55aa34
Hussine <spcocmain@gmail.com>
2020-07-12 14:46:19 +0530

    Changes made to push to heroku

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ yargs.version('1.1.0');
     }
 }) */
 
- const url =`http://api.weatherstack.com/current?access_key=0274be5cb2f9dae9e7d1560363a62b4d&query=${process.argv[2]}&units=m`;
+ const url =`http://api.weatherstack.com/current?access_key=0274be5cb2f9dae9e7d1560363a62b4d&query=Bangalore&units=m`;
 request({url:url,json:true},(error,response)=>{
     console.log(`It's currently ${response.body.current.temperature} celcius and has a ${response.body.current.precip} of rain`);
 }); 
